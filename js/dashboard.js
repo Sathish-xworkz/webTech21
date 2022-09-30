7c483c4290eec230f2aadb41d7eddc62b5037422
sathish <satish.xworkz@gmail.com>
2022-09-30 09:29:22 +0530

    day30-09-22 js class files

---
+++ js/dashboard.js
@@ -0,0 +1,10 @@
+var w="wel";
+var c="come";
+
+console.log(w+c);
+var n=90;
+for(let i=1;i<=n;i++){
+    console.log(i+1);
+}
+console.log("year"+new Date().getFullYear());
+alert("welcome our page")
